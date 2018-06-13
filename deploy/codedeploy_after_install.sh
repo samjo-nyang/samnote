f2b5537095a4ffad79450db0353f828f236ef67c
Sam Jo <samjo.nyang@gmail.com>
2018-06-13 23:28:38 +0900

    add codedeploy travis settings

--- deploy/codedeploy_after_install.sh
+++ deploy/codedeploy_after_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-$SOURCE_STATIC=/data/deploy/dist/
-$TARGET_STATIC=/data/web/static/
+SOURCE_STATIC=/data/deploy/samnote/dist/
+TARGET_STATIC=/data/web/static/
 
 rm -rf $TARGET_STATIC
 cp -r $SOURCE_STATIC $TARGET_STATIC
