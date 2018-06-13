a5103c563d767ab60a48ab3dbf36f3d93a089ed0
Sam Jo <samjo.nyang@gmail.com>
2018-06-13 22:51:33 +0900

    fix zip structure

--- deploy/travis_before_deploy.sh
+++ deploy/travis_before_deploy.sh
@@ -7,7 +7,8 @@ DIR_UPLOAD="$DIR_BASE/tmp-upload"
 if [[ ! -d "$DIR_ZIP" ]]; then
   mkdir $DIR_ZIP $DIR_UPLOAD
   cp -r $DIR_BASE/frontend/dist/ $DIR_ZIP
-  cp -r $DIR_BASE/deploy/ $DIR_ZIP
+  cp -r $DIR_BASE/deploy/* $DIR_ZIP
 
-  zip -r $DIR_UPLOAD/$TRAVIS_COMMIT.zip $DIR_ZIP
+  cd $DIR_ZIP
+  zip -r $DIR_UPLOAD/$TRAVIS_COMMIT.zip *
 fi
