8854e1c9f3a1ef062a396bc24df2d32e686f15e9
Sam Jo <samjo.nyang@gmail.com>
2018-06-13 23:02:09 +0900

    add static copy code to codedeploy_after_intall.sh

--- deploy/codedeploy_after_install.sh
+++ deploy/codedeploy_after_install.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
 
+$SOURCE_STATIC=/data/deploy/dist/
+$TARGET_STATIC=/data/web/static/
+
+rm -rf $TARGET_STATIC
+cp -r $SOURCE_STATIC $TARGET_STATIC
+chown -R www-data:www-data $TARGET_STATIC
+
+systemctl reload nginx
